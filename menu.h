f98a5db389c56555e2024876ee3d93b32f5b5c4c
Erhjiu <2912066768@qq.com>
2025-06-05 00:16:13 +0800

    建立FiterBar

--- menu.h
+++ menu.h
@@ -1,11 +1,14 @@
 #pragma once
-#include"data.h"
+#include "data.h"
 #include "add.h"
 #include <easyx.h>
 #include <vector>
 #include <string>
 #include <ctime>
 #include <iostream>
+
+// 前向声明
+class FilterBar;
 
 using namespace std;
 
@@ -15,53 +18,40 @@ private:
 	vector<GameInfo> games;
 	vector<GameInfo> allGames;
 	UITheme theme;
-	// 过滤器
+	// 筛选器
 	string filter = "全部";
-	// 排列方式
+	// 选择方式
 	int selectedIndex = -1;
 	int hoveredIndex = -1;
 	bool showDetails = false;
-	addButton* addBtn;
+	addButton *addBtn;
 	size_t currentPage = 0;
 	size_t pageSize = 6;
+	FilterBar *filterBar; // 添加FilterBar指针成员
 
 	void LoadSampleData();
 	vector<GameInfo> GetPage(size_t pageIndex);
 	bool LoadGames();
+	friend class FilterBar;
+
+public:
 	// 绘制圆角矩形
 	inline void DrawRoundRect(int x1, int y1, int x2, int y2, int radius, COLORREF fill, COLORREF outline);
-public:
-	friend void to_json(json& j, const GameInfo& game);
-	friend void from_json(const json& j, GameInfo& game);
-	void DrawGameCard(const GameInfo& game, int x, int y, int width, int height, bool isHovered, bool isSelected);
-	//筛选
+	void DrawGameCard(const GameInfo &game, int x, int y, int width, int height, bool isHovered, bool isSelected);
+	// 筛选
 	void categroy();
 	vector<GameInfo> GetCurrentPage();
 	size_t pagePlus(size_t currentPage);
 	size_t pageSub(size_t currentPage);
 
-	//绘制详情面板
-	void DrawDetailPanel(const GameInfo& game, const UITheme& theme);
-	//绘制主界面
+	// 绘制详情面板
+	void DrawDetailPanel(const GameInfo &game, const UITheme &theme);
+	// 绘制主视图
 	void DrawMainView();
 
 	void askGameInfo();
 
-	GameLauncherUI(){
-		LoadSampleData();
-	}
-	~GameLauncherUI() {
-		if (addBtn) {
-			delete addBtn;
-			addBtn = nullptr;
-		}
-		games.clear();
-		allGames.clear();
-		selectedIndex = -1;
-		hoveredIndex = -1;
-		showDetails = false;
-		currentPage = 0;
-		pageSize = 6;
-	}
+	GameLauncherUI();
+	~GameLauncherUI();
 	void run();
 };
